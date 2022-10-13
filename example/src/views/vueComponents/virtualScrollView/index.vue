17f562ed315c70cf8119a670305d9ce964912c6e
biancangming <942242856@qq.com>
2022-10-13 10:18:56 +0800

    增加虚拟滚动自动滚动的适配

--- example/src/views/vueComponents/virtualScrollView/index.vue
+++ example/src/views/vueComponents/virtualScrollView/index.vue
@@ -6,8 +6,20 @@
       <virtualScrollCode />
     </template>
   </CodeView>
+  <CodeView title="虚拟自动滚动">
+    <virtualScrollDemo2 />
+    <template #desc>
+    不太喜欢这种滚动，不过许多业务，尤其是大屏，需要这种效果，一直不知道有什么暖用
+    <p>auto-scroll 表示滑动速度，值越小，滑动越快。 是Boolean 或者一个 Number ， Number 代表滚动的毫秒数，默认是1000ms</p>
+    </template>
+    <template #code>
+      <virtualScrollCode2 />
+    </template>
+  </CodeView>
 </template>
 <script lang="ts" setup>
 import virtualScrollDemo from "./demo/index.vue";
 import virtualScrollCode from "./demo/index.md";
+import virtualScrollDemo2 from "./demo2/index.vue";
+import virtualScrollCode2 from "./demo2/index.md";
 </script>

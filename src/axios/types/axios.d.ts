aaa66b5d96fa5d7e756b98d3d6087ffa1b952341
biancangming <942242856@qq.com>
2023-09-21 11:21:19 +0800

    修改axios.d.ts类型

--- src/axios/types/axios.d.ts
+++ src/axios/types/axios.d.ts
@@ -4,22 +4,22 @@ declare type RequestInterceptor = (config: AxiosRequestConfig) => AxiosRequestCo
 declare type ResponseInterceptor = (response: AxiosResponse) => AxiosResponse
 declare type ErrResponseInterceptor = (err: any) => any
 
-declare interface UseAxiosRequestConfig extends AxiosRequestConfig {
+declare interface HowAxiosRequestConfig extends AxiosRequestConfig {
   path?: any
 }
 
-declare interface UseAxiosInstance extends AxiosRequestConfig {
+declare interface HowAxiosInstance extends AxiosRequestConfig {
 }
 
 // 额外参数
-declare interface UseExRequestOptions {
+declare interface HowExRequestOptions {
   immediate?: boolean, // 是否立即执行
   delay?: number, // 防抖延迟时间
   isDebounce?: boolean, // 是否防抖
   defaultVal?: any
 }
 
-declare interface UseDownLoadExRequestOptions extends UseExRequestOptions {
+declare interface HowDownLoadExRequestOptions extends HowExRequestOptions {
     fileName?: string, // 文件名称
     contentType?: contentTypeStr, // 文件类型
     cbdata?: (res: AxiosResponse) => any, // 返回值处理，默认取 response.data
